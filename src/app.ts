208dd167d6f9ce7a3d617bb9c3b765edbfef27dc
Michael Wildman <michael@mwild.me>
2016-08-13 15:32:58 +1200

    simple (naieve) translation

--- src/app.ts
+++ src/app.ts
@@ -14,47 +14,79 @@ gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 // get uniforms
 let u_resolution = gl.getUniformLocation(program, "u_resolution");
 let u_color = gl.getUniformLocation(program, "u_color");
+let u_translation = gl.getUniformLocation(program, "u_translation");
 
 // get attributes
 let a_position = GlAttribute.get(program, "a_position", 2);
-
 
 // set resolution
 gl.uniform2f(u_resolution, gl.canvas.width, gl.canvas.height);
 
 
+let x = 0;
+let x_input = <HTMLInputElement> document.getElementById("gl-x");
+x_input.value = String(x);
+
+let y = 0;
+let y_input = <HTMLInputElement> document.getElementById("gl-y");
+y_input.value = String(y);
 
 
 
-// create a rectangle
-function setRectangle(gl, x, y, width, height) {
-    var x1 = x;
-    var x2 = x + width;
-    var y1 = y;
-    var y2 = y + height;
 
+
+function setGeometry() {
     return new Float32Array([
-        x1, y1,
-        x2, y1,
-        x1, y2,
-        x1, y2,
-        x2, y1,
-        x2, y2]);
+        // left column
+        0, 0,
+        30, 0,
+        0, 150,
+        0, 150,
+        30, 0,
+        30, 150,
+
+        // top rung
+        30, 0,
+        100, 0,
+        30, 30,
+        30, 30,
+        100, 0,
+        100, 30,
+
+        // middle rung
+        30, 60,
+        67, 60,
+        30, 90,
+        30, 90,
+        67, 60,
+        67, 90
+        ]);
 }
 
-// bind the position attribute to gl.ARRAY_BUFFER
-a_position.bind();
+function drawScene() {
+    // bind the position attribute to gl.ARRAY_BUFFER
+    a_position.bind();
 
-// buffer 50 random rectangles in random colors
-for (var ii = 0; ii < 50; ++ii) {
-
-
-    // set up random rectangle
-    a_position.data = setRectangle(gl, util.randomInt(300), util.randomInt(300), util.randomInt(300), util.randomInt(300));
+    // set up the scene
+    a_position.data = setGeometry();
     a_position.buff();
 
     // set up random color
     gl.uniform4f(u_color, Math.random(), Math.random(), Math.random(), 1);
 
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
+    gl.drawArrays(gl.TRIANGLES, 0, (a_position.data.length / a_position.size));
+
 }
+
+
+function update() {
+    x = Number(x_input.value);
+    y = Number(y_input.value);
+
+    gl.uniform2f(u_translation, x, y);
+
+    drawScene(); 
+}
+
+
+update();
